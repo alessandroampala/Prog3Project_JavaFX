f032b3381c5c7378d0b06ce97a12b063e58b4a64
Emilio Bruno <emilio.bruno@outlook.it>
2020-12-12 13:57:49 +0100

    Added socket communication to send email

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -11,13 +11,22 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
+import mail.Email;
+import mail.Request;
+import server.RequestHandler;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.regex.Pattern;
 
 public class Client {
+    private static final String ADDRESS = "localhost";
+    private static final int PORT = 9000;
 
     @FXML
     private Label username;
@@ -30,10 +39,9 @@ public class Client {
     @FXML
     private TextArea message;
 
-    private JFXSnackbar jfxSnackbar;
-
     @FXML
     public void initialize() {
+
     }
 
     @FXML
@@ -66,7 +74,36 @@ public class Client {
             System.out.println("message");
             return;
         }
-        System.out.println("SENT");
+        Request send = new Request("Send email", new Email(-1, username.getText(), Arrays.asList(emails), subject.getText(), message.getText(), new Date()));
+        sendRequest(send);
+    }
+
+    private void sendRequest(Request send) {
+        new Thread(() -> {
+            Socket socket = null;
+            try {
+                socket = new Socket(ADDRESS, PORT);
+
+                Scanner in = new Scanner(socket.getInputStream());
+
+                ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
+                outStream.writeObject(send);
+
+                while (in.hasNext()) {
+                    System.out.println(in.nextLine());
+                }
+            } catch (IOException e) {
+                System.out.println("Connection Error");
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        System.out.println("Error during socket disconnection");
+                    }
+                }
+            }
+        }).start();
     }
 
     public void readMail() {
