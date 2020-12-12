08fcb0e92b13f138f5b19db60d2fb595179f4193
Emilio Bruno <emilio.bruno@outlook.it>
2020-12-12 14:17:00 +0100

    Removed material design

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
@@ -36,7 +37,7 @@ public class Client {
     @FXML
     private HBox unselectedMailContainer;
     @FXML
-    private JFXTextField to, subject;
+    private TextField to, subject;
     @FXML
     private TextArea message;
 
